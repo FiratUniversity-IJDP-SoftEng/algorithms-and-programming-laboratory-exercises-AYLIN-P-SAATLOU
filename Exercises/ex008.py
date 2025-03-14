62d6bbc114aaac934fb47f79f40a61db6a5e28fa
Aylin prm <aylin@Aylin-MacBook-Air.local>
2025-03-14 13:22:25 +0300

    exc8 complete

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,8 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 240543604
+# Your Name and Surname: Aylin-P-Saatlou
+
+n = int(input("Enter the number of rows: "))
+
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    print("*" * (2 * i - 1))
